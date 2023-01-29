2ffdb85b54cbe4675a29fb5693dacc5a0df3a294
Jacobo91 <jacobovelasquez1991@gmail.com>
2023-01-29 18:03:26 -0500

    add projects gallery and footer

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const mediaLogos = document.querySelectorAll('[data-media]');
+const emailInput = document.querySelector("[data-email]");
 
 const log = arg => {
     console.log(arg);
@@ -8,6 +9,23 @@ const bounceLogo = () => {
     event.target.classList.toggle('animate__pulse')
 }
 
+const checkEmail = () => {
+    console.log(event.target.value);
+}
+
 log(mediaLogos)
 
 mediaLogos.forEach(logo => logo.addEventListener('mouseover', bounceLogo));
+
+
+
+// Listen for the input's input event
+emailInput.addEventListener("input", function() {
+  if (!emailInput.validity.valid) {
+    emailInput.classList.add("invalid");
+  } else {
+    emailInput.classList.remove("invalid");
+  }
+});
+
+
